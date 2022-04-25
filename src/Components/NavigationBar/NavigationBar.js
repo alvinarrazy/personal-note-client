a65e767c49ebff9223328525ee2b86e5469c1c35
Alvin Arrazy <alvinarrazy29@gmail.com>
2022-04-26 04:51:54 +0700

    added brand

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -6,11 +6,11 @@ export default function NavigationBar() {
     return (
         <Navbar bg="light" expand="lg">
             <Container fluid>
-                <Navbar.Brand to="/"></Navbar.Brand>
+                <Navbar.Brand className='ml-6' to="/">Aqila's Personal Note</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse style={{ marginRight: 0 }} id="basic-navbar-nav">
                     <Nav className="me-auto align-items-center">
-                        <Link className='nav-link' to="/">Home</Link>
+                        <Link className='nav-link' to="/">Assignment List</Link>
                         <Link className='nav-link ml-4' to="/new">
                             <Button variant="primary">New assignment!</Button>
                         </Link>
